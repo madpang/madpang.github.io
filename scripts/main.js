fb9fda6629287a5423108a639c284dff8e94a262
TPP <alienkiller3@me.com>
2022-08-11 11:29:06 +0900

    Syntax error fix

--- scripts/main.js
+++ scripts/main.js
@@ -8,25 +8,3 @@ myImage.onclick = function() {
 		myImage.setAttribute('src', './images/LOGO-ver-04.png');
 	}
 }
-
-// let myButton = document.querySelector('button');
-// let myHeading = document.querySelector('h1');
-
-// function setUserName() {
-// 	let myName = prompt('Please enter your name: ');
-// 	if(!myName) {
-// 		setUserName();
-// 	} else {
-// 		localStorage.setItem('name', myName);
-// 		myHeading.textContent = 'Welcome to the wild, ' + myName + '!';
-// 	}
-// }
-
-// if(!localStorage.getItem('name')) {
-// 	setUserName();
-// } else {
-// 	let storedName = localStorage.getItem('name');
-// 	myHeading.textContent = 'Welcome to the wild, ' + storedName + '!';
-// }
-
-// myButton.onclick = setUserName;

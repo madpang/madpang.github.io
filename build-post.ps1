a489613a04211537e4840cf7ae9c710fbdfe3443
madpang <133752527+madpang@users.noreply.github.com>
2025-05-22 00:12:26 +0900

    Extract the article title for display on webpage tab

--- build-post.ps1
+++ build-post.ps1
@@ -21,31 +21,46 @@ param(
 	[Parameter(Mandatory = $True, Position = 3)][string]$path2html_template
 )
 
-$converter = "./tools/mmd2html/mmd2html"
+$converter_ = "./tools/mmd2html/mmd2html"
 
 # === Convert the source text to HTML
-$formatted_lines = (& $converter $path2txt)
+$formatted_lines = (& $converter_ $path2txt)
+
+# --- Extract h1 heading (for display in the tab title)
+$tab_title_ = $null
+foreach ($line in $formatted_lines) {
+	if ($line -match '<h1>(.*)</h1>') {
+		$tab_title_ = $Matches[1]
+		break
+	}
+}
+if (-not $tab_title_) {
+	Write-Error "ERROR: article must have a title."
+	exit 2
+}
+
+$author_info_ = "MadPang"
 
 # === Read the template HTML file
-$t_lines = Get-Content -Path $path2html_template
+$t_lines_ = Get-Content -Path $path2html_template
 
 # === Initialize the output HTML
-$out_lines = @()
+$out_lines_ = @()
 
 # === Processing
 $inBlockComment = $false
-for ($ii = 0; $ii -lt $t_lines.Count; $ii++) {
-	$line = $t_lines[$ii]
+for ($ii = 0; $ii -lt $t_lines_.Count; $ii++) {
+	$line = $t_lines_[$ii]
 	# Check if the line is a block comment start, assuming only world character, `@`, and `:` are allowed
-	if ($line -match "<!--\s*(?<id>[@:\w]+)\s*$") {
+	if ($line -match '^<!--\s*(?<id>[@:\w]+)\s*$') {
 		$inBlockComment = $true
 		$id = $Matches['id']
-		if ($id -eq "@ANCHOR:NULL") {
-			$out_lines += '<!-- @note: This file is auto-generated. MANUAL EDITS WILL BE LOST -->'
+		if ($id -eq '@ANCHOR:NULL') {
+			$out_lines_ += '<!-- @note: This file is auto-generated. MANUAL EDITS WILL BE LOST -->'
 		}
 		continue
 	}
-	if ($line -eq '-->') {
+	if ($line -match '^-->$') {
 		$inBlockComment = $false
 		continue
 	}
@@ -53,14 +68,20 @@ for ($ii = 0; $ii -lt $t_lines.Count; $ii++) {
 		# Skip the block comment
 		continue
 	}
+	if ($line -match '^<!--\s*@ANCHOR:TITLE\s*-->$') {
+		# Insert the title text into the anchor point
+		$out_lines_ += "$tab_title_ | $author_info_"
+		continue
+	}
 	if ($line -match "^<!--\s*@ANCHOR:ARTICLE\s*-->$") {
 		# Insert the formatted lines into the anchor point
-		$out_lines += $formatted_lines
-	} else {
-		# Add the line to the output
-		$out_lines += $line
+		$out_lines_ += $formatted_lines
+		continue
 	}
+
+	# Add normal line to the output
+	$out_lines_ += $line
 }
 
 # === Write the output to HTML file
-Set-Content -Path $path2html -Value $out_lines -Encoding utf8
+Set-Content -Path $path2html -Value $out_lines_ -Encoding utf8
